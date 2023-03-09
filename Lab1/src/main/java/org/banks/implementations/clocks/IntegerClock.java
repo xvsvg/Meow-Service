3401ec0ceab049c19eac1de6535f87cdaff8367c
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-03-10 00:24:43 +0300

    feat: changed constructor restrictions

--- Lab1/src/main/java/org/banks/implementations/clocks/IntegerClock.java
+++ Lab1/src/main/java/org/banks/implementations/clocks/IntegerClock.java
@@ -3,6 +3,7 @@ package org.banks.implementations.clocks;
 import io.reactivex.rxjava3.core.Observable;
 import org.banks.implementations.banks.CentralBank;
 import org.banks.contracts.clocks.Runnable;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * {@inheritDoc}
@@ -10,6 +11,8 @@ import org.banks.contracts.clocks.Runnable;
 public class IntegerClock implements Runnable {
 
     private int timeOffset;
+
+    @Nullable
     private CentralBank bank;
     private final int timeToWait;
     private final int notifyFrequency;
@@ -20,7 +23,7 @@ public class IntegerClock implements Runnable {
      * @param timeBetweenTicksInMilleseconds time between ticks in milleseconds
      * @param notifyFrequency time between months in milleseconds
      */
-    public IntegerClock(CentralBank bank, int timeBetweenTicksInMilleseconds, int notifyFrequency) {
+    public IntegerClock(@Nullable CentralBank bank, int timeBetweenTicksInMilleseconds, int notifyFrequency) {
         this.bank = bank;
         this.timeToWait = timeBetweenTicksInMilleseconds;
         this.notifyFrequency = notifyFrequency;
