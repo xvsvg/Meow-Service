9230648f553a98444e9608df81472d7cd02de304
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-03-24 20:20:16 +0300

    feat: modified dependencies

--- Lab2/services/build.gradle.kts
+++ Lab2/services/build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     implementation(project(":Lab2:dao"))
     implementation(project(":Lab2:domain"))
     implementation("javax.persistence:javax.persistence-api:2.2")
+    implementation(project(mapOf("path" to ":Lab2:dto")))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
