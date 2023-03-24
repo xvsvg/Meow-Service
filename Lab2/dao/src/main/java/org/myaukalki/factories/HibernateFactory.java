5cbc3a79c4e816364ad612aa6d51b180b7bcdb23
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-03-24 20:23:39 +0300

    feat: add annotated hierarchy classes

--- Lab2/dao/src/main/java/org/myaukalki/factories/HibernateFactory.java
+++ Lab2/dao/src/main/java/org/myaukalki/factories/HibernateFactory.java
@@ -1,9 +1,15 @@
 package org.myaukalki.factories;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.myaukalki.domain.contracts.*;
+import org.myaukalki.domain.implementations.CatImpl;
+import org.myaukalki.domain.implementations.OwnerImpl;
+
+import java.io.File;
 
 public class HibernateFactory {
 
@@ -15,15 +21,17 @@ public class HibernateFactory {
     public static SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
             try {
-                Configuration configuration = new Configuration().configure("../../../../../hibernate.cfg.xml");
+                Configuration configuration = new Configuration().configure();
                 configuration.addAnnotatedClass(Owner.class);
                 configuration.addAnnotatedClass(Pet.class);
+                configuration.addAnnotatedClass(CatImpl.class);
+                configuration.addAnnotatedClass(OwnerImpl.class);
 
                 StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
                         .applySettings(configuration.getProperties());
 
-                sessionFactory = configuration.buildSessionFactory(builder.build());
 
+                sessionFactory = configuration.buildSessionFactory(builder.build());
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
