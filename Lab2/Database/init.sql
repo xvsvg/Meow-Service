65b2d801c419565f128c9bc0320e67f8092db78f
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-03-24 20:21:22 +0300

    feat: modified init.sql

--- Lab2/Database/init.sql
+++ Lab2/Database/init.sql
@@ -3,29 +3,29 @@ CREATE DATABASE tech_database;
 
 \c tech_database;
 
-CREATE TABLE IF NOT EXISTS Owners
+CREATE TABLE IF NOT EXISTS owner
 (
-    OwnerID        INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    OwnerName      VARCHAR(255) NOT NULL,
-    OwnerBirthDate DATE         NOT NULL
+    owner_id        INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    owner_name      VARCHAR(255) NOT NULL,
+    owner_birth_date DATE         NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Pets
+CREATE TABLE IF NOT EXISTS pet
 (
-    PetID        INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    PetName      VARCHAR(255) NOT NULL,
-    PetBirthDate DATE         NOT NULL,
-    OwnerID      INT,
-    CONSTRAINT OwnerID FOREIGN KEY (OwnerID) REFERENCES Owners (OwnerID),
-    Breed        VARCHAR(255) NOT NULL,
-    Color        VARCHAR(255) NOT NULL
+    pet_id        INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    pet_name      VARCHAR(255) NOT NULL,
+    pet_birth_date DATE         NOT NULL,
+    owner_id      INT,
+    CONSTRAINT owner_id FOREIGN KEY (owner_id) REFERENCES owner (owner_id),
+    breed        VARCHAR(255) NOT NULL,
+    color        VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Friends
+CREATE TABLE IF NOT EXISTS friend
 (
-    Pet1ID INTEGER,
-    Pet2ID INTEGER,
-    CONSTRAINT Pet1ID FOREIGN KEY (Pet1ID) REFERENCES Pets (PetID),
-    CONSTRAINT Pet2ID FOREIGN KEY (Pet1ID) REFERENCES Pets (PetID),
-    PRIMARY KEY (Pet1ID, Pet2ID)
+    pet_id INTEGER,
+    friend_id INTEGER,
+    CONSTRAINT pet_id FOREIGN KEY (pet_id) REFERENCES pet (pet_id) ON DELETE CASCADE,
+    CONSTRAINT friend_id FOREIGN KEY (friend_id) REFERENCES pet (pet_id) ON DELETE CASCADE,
+    PRIMARY KEY (pet_id, friend_id)
 );
