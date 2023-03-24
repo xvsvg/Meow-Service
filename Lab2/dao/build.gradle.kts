3d56ab7e00df185ed6f55600fc61fb13a9c65d72
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-03-24 20:20:24 +0300

    feat: modified dependencies

--- Lab2/dao/build.gradle.kts
+++ Lab2/dao/build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     compileOnly("org.projectlombok:lombok:1.18.22")
     annotationProcessor("org.projectlombok:lombok:1.18.22")
     implementation("org.hibernate:hibernate-gradle-plugin:5.6.15.Final")
+    implementation("org.postgresql:postgresql:42.5.4")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
