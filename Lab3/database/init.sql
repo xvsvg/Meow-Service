ca7ce0dc8d7fae9b13c38e2f11b39c09a2240f38
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-03-29 03:35:03 +0300

    feat: cleanup in init.sql

--- Lab3/database/init.sql
+++ Lab3/database/init.sql
@@ -1,16 +1,2 @@
 DROP DATABASE IF EXISTS tech_database;
 CREATE DATABASE tech_database;
-
--- \c tech_database;
---
--- CREATE TABLE IF NOT EXISTS owners
--- (
--- );
---
--- CREATE TABLE IF NOT EXISTS cats
--- (
--- );
---
--- CREATE TABLE IF NOT EXISTS friends
--- (
--- );
