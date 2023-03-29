9b9ad801dbdb521a5a3bc4072fc3df6b47c4f88e
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-03-29 03:29:48 +0300

    feat: add pagination and query parameter supply

--- Lab3/backend/Lab3/src/main/java/org/meows/controllers/CatController.java
+++ Lab3/backend/Lab3/src/main/java/org/meows/controllers/CatController.java
@@ -1,22 +1,26 @@
 package org.meows.controllers;
 
 import org.meows.exceptions.CatServiceException;
-import org.meows.models.CatResponse;
-import org.meows.models.CreateCatRequest;
-import org.meows.models.UpdateCatRequest;
+import org.meows.models.*;
+import org.meows.models.create.CreateCatRequest;
+import org.meows.models.filter.CatFilterRequest;
+import org.meows.models.get.CatGetResponse;
+import org.meows.models.page.CatPageResponse;
+import org.meows.models.update.UpdateCatRequest;
 import org.meows.services.CatService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 public class CatController {
 
+    private final Integer DATA_PER_PAGE = 20;
     private CatService catService;
     private Logger logger;
 
@@ -27,11 +31,10 @@ public class CatController {
     }
 
     @GetMapping("/api/cats/{id}")
-    public ResponseEntity<CatResponse> getCatById(@PathVariable("id") Long id) {
-        try{
+    public ResponseEntity<CatGetResponse> getCatById(@PathVariable("id") Long id) {
+        try {
             return new ResponseEntity<>(catService.getById(id), HttpStatus.OK);
-        }
-        catch(CatServiceException e){
+        } catch (CatServiceException e) {
             logger.error(e.getMessage());
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
@@ -39,16 +42,24 @@ public class CatController {
     }
 
     @GetMapping("/api/cats")
-    public ResponseEntity<List<CatResponse>> getAllCats() {
-            return new ResponseEntity<>(catService.getAllCats(), HttpStatus.OK);
+    public ResponseEntity<CatPageResponse> getAllCats(@RequestParam(value = "page", required = false, defaultValue = "1") Integer page,
+                                                      @RequestParam(value = "size", required = false, defaultValue = "20") Integer size,
+                                                      @ModelAttribute CatFilterRequest request) {
+        try {
+            Pageable info = PageRequest.of(page - 1, size);
+            return new ResponseEntity<>(catService.getAllCats(info, request), HttpStatus.PARTIAL_CONTENT);
+        } catch (RuntimeException e) {
+            logger.error(e.getMessage());
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.PAYLOAD_TOO_LARGE);
+        }
     }
 
     @PostMapping("/api/cats/create")
     public ResponseEntity<CatResponse> createCat(@RequestBody CreateCatRequest request) {
-        try{
-            return new ResponseEntity<>(catService.create(request), HttpStatus.OK);
-        }
-        catch(CatServiceException e){
+        try {
+            return new ResponseEntity<>(catService.create(request), HttpStatus.CREATED);
+        } catch (CatServiceException e) {
             logger.error(e.getMessage());
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -57,10 +68,9 @@ public class CatController {
 
     @DeleteMapping("/api/cats/delete/{id}")
     public ResponseEntity<CatResponse> deleteCat(@PathVariable("id") Long id) {
-        try{
+        try {
             return new ResponseEntity<>(catService.deleteById(id), HttpStatus.OK);
-        }
-        catch(CatServiceException e){
+        } catch (CatServiceException e) {
             logger.error(e.getMessage());
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -69,10 +79,9 @@ public class CatController {
 
     @PutMapping("/api/cats/update")
     public ResponseEntity<CatResponse> updateCat(@RequestBody UpdateCatRequest request) {
-        try{
+        try {
             return new ResponseEntity<>(catService.update(request), HttpStatus.OK);
-        }
-        catch(CatServiceException e){
+        } catch (CatServiceException e) {
             logger.error(e.getMessage());
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -81,10 +90,9 @@ public class CatController {
 
     @PostMapping("/api/cats/{id}/friend/{friendId}")
     public ResponseEntity<CatResponse> addFriend(@PathVariable("id") Long id, @PathVariable("friendId") Long friendId) {
-        try{
+        try {
             return new ResponseEntity<>(catService.addFriend(id, friendId), HttpStatus.OK);
-        }
-        catch(CatServiceException e){
+        } catch (CatServiceException e) {
             logger.error(e.getMessage());
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -93,10 +101,9 @@ public class CatController {
 
     @DeleteMapping("/api/cats/{id}/unfriend/{friendId}")
     public ResponseEntity<CatResponse> removeFriend(@PathVariable("id") Long id, @PathVariable("friendId") Long friendId) {
-        try{
+        try {
             return new ResponseEntity<>(catService.removeFriend(id, friendId), HttpStatus.OK);
-        }
-        catch(CatServiceException e){
+        } catch (CatServiceException e) {
             logger.error(e.getMessage());
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
--- Lab3/backend/Lab3/src/main/java/org/meows/controllers/OwnerController.java
+++ Lab3/backend/Lab3/src/main/java/org/meows/controllers/OwnerController.java
@@ -1,18 +1,20 @@
 package org.meows.controllers;
 
 import org.meows.exceptions.OwnerServiceException;
-import org.meows.models.CreateOwnerRequest;
+import org.meows.models.create.CreateOwnerRequest;
+import org.meows.models.get.OwnerGetResponse;
+import org.meows.models.page.OwnerPageResponse;
 import org.meows.models.OwnerResponse;
-import org.meows.models.UpdateOwnerRequest;
+import org.meows.models.update.UpdateOwnerRequest;
 import org.meows.services.OwnerService;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 public class OwnerController {
@@ -28,7 +30,7 @@ public class OwnerController {
     }
 
     @GetMapping("/api/owners/{id}")
-    public ResponseEntity<OwnerResponse> getOwnerById(@PathVariable("id") Long id) {
+    public ResponseEntity<OwnerGetResponse> getOwnerById(@PathVariable("id") Long id) {
         try {
             return new ResponseEntity<>(ownerService.getById(id), HttpStatus.OK);
         } catch (OwnerServiceException e) {
@@ -39,14 +41,23 @@ public class OwnerController {
     }
 
     @GetMapping("/api/owners")
-    public ResponseEntity<List<OwnerResponse>> getAllOwners() {
-        return new ResponseEntity<>(ownerService.getAll(), HttpStatus.OK);
+    public ResponseEntity<OwnerPageResponse> getAllOwners(@RequestParam(value = "page", required = false, defaultValue = "1") Integer page,
+                                                          @RequestParam(value = "size", required = false, defaultValue = "20") Integer size) {
+
+        try {
+            Pageable info = PageRequest.of(page - 1, size);
+            return new ResponseEntity<>(ownerService.getAll(info), HttpStatus.PARTIAL_CONTENT);
+        } catch (RuntimeException e) {
+            logger.error(e.getMessage());
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.PAYLOAD_TOO_LARGE);
+        }
     }
 
     @PostMapping("/api/owners/create")
     public ResponseEntity<OwnerResponse> createOwner(@RequestBody CreateOwnerRequest owner) {
         try {
-            return new ResponseEntity<>(ownerService.create(owner), HttpStatus.OK);
+            return new ResponseEntity<>(ownerService.create(owner), HttpStatus.CREATED);
         } catch (OwnerServiceException e) {
             logger.error(e.getMessage());
             e.printStackTrace();
