a348e9791b9cffb38a61296a55d8aae3d103d8f0
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-04-01 01:53:55 +0300

    feat: add tables

---
+++ Lab3/frontend/src/Components/Tables/OwnerTable.tsx
@@ -0,0 +1,31 @@
+import { Table } from "react-bootstrap";
+import { IOwner } from "../SearchForms/GetOwnerForm";
+
+interface OwnerTableProps {
+	users: IOwner[]
+}
+
+export function OwnerTable({ users }: OwnerTableProps) {
+	return (
+		<>
+			<Table striped bordered hover>
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>Name</th>
+						<th>Email</th>
+					</tr>
+				</thead>
+				<tbody>
+					{users.map((user) => (
+						<tr key={user.id}>
+							<td>{user.id}</td>
+							<td>{user.name}</td>
+							<td>{user.birthDate}</td>
+						</tr>
+					))}
+				</tbody>
+			</Table>
+		</>
+	)
+}
